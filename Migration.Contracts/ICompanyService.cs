ed19e83e57add91befc2e682ecd6169186bd4192
mabramovsd <beavisabra@yandex.ru>
2026-06-26 13:19:24 +0300

    Project with contracts

---
+++ Migration.Contracts/ICompanyService.cs
@@ -0,0 +1,18 @@
+﻿namespace Migration.Contracts
+{
+    /// <summary>
+    /// Interface for company
+    /// </summary>
+    public interface ICompanyService
+    {
+        /// <summary>
+        /// Hire employee to the company
+        /// </summary>
+        public bool AddEmployee(IEmployee employee);
+
+        /// <summary>
+        /// Fire employee from the company
+        /// </summary>
+        public bool RemoveEmployee(IEmployee employee);
+    }
+}
---
+++ Migration.Contracts/IEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migration.Contracts
+{
+    /// <summary>
+    /// Interface for employee
+    /// </summary>
+    public interface IEmployee
+    {
+        /// <summary>
+        /// Employee Identifier (unique)
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Employee name
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
